ee67db4f6ec0024f2d7ad147d6bb260f7ea91a94
solovino <montoya8@gmail.com>
2015-01-20 21:26:25 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,48 @@
 ## The functions below cache the inverse of a matrix rather than compute it repeatedly.
 ## See each function for more details.
 
-
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	# Creates a special "matrix" object that can cache 
+	# its inverse.
+	# x: is a matrix object
+	# Returns: a list that makes up the special "matrix" 
+	# object with elements "get" which retrieves the 
+	# matrix, "setinv" which the assigns the object its 
+	# inverse, and "getinv" which retrieves the inverse
+	# of the matrix.
+        m <- NULL 
+        get <- function() x  
+        setinv <- function(inv) m <<- inv  
+        getinv <- function() m   
+        list(get = get, setinv = setinv,
+             getinv = getinv )	
 }
 
+ 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" object
+## returned by makeCacheMatrix above. If the inverse has already 
+## been computed (and the matrix has not changed), then  
+## cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Retrieves or computes the inverse of x.
+	# x: is a matrix object
+	# Returns: the inverse of x 
+		
+		m <- x$getinv()  # retrieve the inverse if it's been 
+						 # computed before
+        if(!is.null(m)) {  
+                message("getting cached matrix inverse")
+                return(m) # return the cached inverse
+        }
+        data <- x$get()   # retrieve the matrix
+        m <- solve(data, ...)  # compute the inverse of the matrix
+        x$setinv(m)   # assign the inverse to the special matrix 
+					  #bject for future usage
+        m # returns the inverse
 }
 
 
